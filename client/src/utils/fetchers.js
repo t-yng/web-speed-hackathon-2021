980568c9046e203e64dbec870f306a8d38a626b4
t-yng <t-yng@users.noreply.github.com>
2021-12-31 01:11:51 +0900

    リクエストを非同期に変更 (#8)

--- client/src/utils/fetchers.js
+++ client/src/utils/fetchers.js
@@ -6,7 +6,6 @@ import { gzip } from 'pako';
  */
 async function fetchBinary(url) {
   const result = await $.ajax({
-    async: false,
     dataType: 'binary',
     method: 'GET',
     responseType: 'arraybuffer',
@@ -22,7 +21,6 @@ async function fetchBinary(url) {
  */
 async function fetchJSON(url) {
   const result = await $.ajax({
-    async: false,
     dataType: 'json',
     method: 'GET',
     url,
@@ -38,7 +36,6 @@ async function fetchJSON(url) {
  */
 async function sendFile(url, file) {
   const result = await $.ajax({
-    async: false,
     data: file,
     dataType: 'json',
     headers: {
@@ -63,7 +60,6 @@ async function sendJSON(url, data) {
   const compressed = gzip(uint8Array);
 
   const result = await $.ajax({
-    async: false,
     data: compressed,
     dataType: 'json',
     headers: {
