993505ae8537f9bef36f72ce1b08feb148bb3dc4
t-yng <t-yng@users.noreply.github.com>
2021-12-31 00:34:38 +0900

    responseをgzip圧縮 (#7)

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser';
 import Express from 'express';
 import session from 'express-session';
+import compression from 'compression';
 
 import { apiRouter } from './routes/api';
 import { staticRouter } from './routes/static';
@@ -19,14 +20,7 @@ app.use(
 );
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ limit: '10mb' }));
-
-app.use((_req, res, next) => {
-  res.header({
-    'Cache-Control': 'max-age=0, no-transform',
-    Connection: 'close',
-  });
-  return next();
-});
+app.use(compression());
 
 app.use('/api/v1', apiRouter);
 app.use(staticRouter);
